abef19e693f27d6affc3b5330f31baf0997bfde5
38093 <nikolaef.post@gmail.com>
2021-06-22 09:13:21 +0300

    SimpleBreadcrumbs

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import styles from '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, Sidebar, Search, ConversationList, Conversation, Avatar } from '@chatscope/chat-ui-kit-react';
 import { useQuery } from '@apollo/client';
 import { GET_FIRST_FIVE_USERS } from "./gql/query"
-// import React from 'react'
+import React from 'react'
 import {
     BrowserRouter as Router,
     Switch,
@@ -16,6 +16,30 @@ import SignInSide from "./pages/signInSide/SignInSide";
 import Dashboard from "./pages/dashboard/Dashboard";
 import Album from "./pages/Album/Album";
 
+
+
+
+
+
+import Typography from '@material-ui/core/Typography';
+import Breadcrumbs from '@material-ui/core/Breadcrumbs';
+
+
+function handleClick(event) {
+    event.preventDefault();
+    console.info('You clicked a breadcrumb.');
+}
+
+ function SimpleBreadcrumbs() {
+    return (
+        <Breadcrumbs aria-label="breadcrumb">
+            <Link to="/">Dashboard</Link>
+            <Link to="/signin">Sign in</Link>
+            <Link to="/album">Album</Link>
+        </Breadcrumbs>
+    );
+}
+
 function App() {
     const { loading, error, data } = useQuery(GET_FIRST_FIVE_USERS);
 
@@ -25,19 +49,7 @@ function App() {
     return (
         <Router>
             <div>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/signin">Sign in</Link>
-                        </li>
-                        <li>
-                            <Link to="/album">Album</Link>
-                        </li>
-                    </ul>
-                </nav>
+                <SimpleBreadcrumbs/>
             <Switch>
                 <Route path='/signin'>
                     <SignInSide/>
